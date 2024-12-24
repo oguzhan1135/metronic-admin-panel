21ac41bcb2a9d4fb18abff85d558ad96a781451b
oguzhan1135 <111190495+oguzhan1135@users.noreply.github.com>
2024-12-24 04:46:41 +0300

    Sub item selected operations - Menu item data fixed #3

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -19,61 +19,259 @@ import Handcart from '@assets/icon/handcart.svg'
 import { useState } from 'react'
 
 
+interface MenuItem {
+    key: string;
+    label: string;
+    subItems?: MenuItem[];
+}
+const menuData: MenuItem[] = [
+    {
+        key: "dashboards",
+        label: "Dashboards",
+        subItems: [
+            {
+                key: "light", label: "Light Sidebar"
+            },
+            {
+                key: "dark", label: "Dark Sidebar"
+            }
+        ]
+    },
+    {
+        key: "public",
+        label: "Public Profile",
+        subItems: [
+            {
+                key: "profiles",
+                label: "Profiles",
+                subItems: [
+                    {
+                        key: "default", label: "Default"
+                    },
+                    {
+                        key: "creator", label: "Creator"
+                    },
+                    {
+                        key: "company", label: "Company"
+                    },
+                    {
+                        key: "nft", label: "NFT"
+                    },
+                    {
+                        key: "blogger", label: "Blogger"
+                    },
+                    {
+                        key: "crm", label: "CRM"
+                    },
+                    {
+                        key: "gamer", label: "Gamer"
+                    },
+                    {
+                        key: "feeds", label: "Feeds"
+                    },
+                    {
+                        key: "plain", label: "PLain"
+                    },
+                    {
+                        key: "modal", label: "Modal"
+                    }
+                ]
+
+            },
+            {
+                key: "projects",
+                label: "Projects",
+                subItems: [
+                    {
+                        key: "column-3", label: "3 Columns"
+                    },
+                    {
+                        key: "column-2", label: "2 Columns"
+                    }
+                ]
+            },
+            {
+                key: "works",
+                label: "Works"
+            },
+            {
+                key: "teams",
+                label: "Teams"
+            },
+            {
+                key: "network",
+                label: "Network"
+            },
+            {
+                key: "activity",
+                label: "Activity"
+            },
+            {
+                key: "chanpaigns-card",
+                label: "Champaigns - Card"
+            },
+            {
+                key: "chanpaigns-list",
+                label: "Champaigns - List"
+            },
+            {
+                key: "empty",
+                label: "Empty"
+            }
+        ]
+    },
+    {
+        key:"my-account",
+        label:"My Account",
+        subItems:[
+            {
+                key:"account-home", 
+                label:"Account Home",
+                subItems:[
+                    {
+                        key:"get-started",label:"Get Started"
+                    },
+                    {
+                        key:"user-profile", label:"User Profile"
+                    },
+                    {
+                        key:"company-profile", label:"Company Profile"
+                    },
+                    {
+                        key:"setting-with-sidebar", label:"Setting - With Sidebar"
+                    },
+                    {
+                        key:"setting-enterprise", label:"Setting - Enterprise"
+                    },
+                    {
+                        key:"setting-plain", label:"Setting - Plain"
+                    },
+                    {
+                        key:"setting-with-modal", label:"Setting - Modal"
+                    }
+                ]
+            },
+            {
+                key:"billing",
+                label:"Billing",
+                subItems:[
+                    {
+                        key:"billing-basic", label:"Billing - Basic"
+                    },
+                    {
+                        key:"billing-enterprise", label:"billing - Enterprise"
+                    },
+                    {
+                        key:"plans", label:"Plans"
+                    },
+                    {
+                        key:"billing-history", label:"Billing History"
+                    }
+                ]
+            },
+            {
+                key:"security",
+                label:"Security",
+                subItems:[
+                    {
+                        key:"get-started", label:"Get Started"
+                    },
+                    {
+                        key:"security-overview", label:"Security Overview"
+                    },
+                    {
+                        key:"allowed-ip-addresses", label:"Allowed IP Addresses"
+                    },
+                    {
+                        key:"privacy-setting", label:"Privacy Setting"
+                    },
+                    {
+                        key:"device-management", label:"Device Management"
+                    },
+                    {
+                        key:"backup-recovery", label:"Backup & Recovery"
+                    },
+                    {
+                        key:"current-sessions", label:"Current Sessions"
+                    },
+                    {
+                        key:"security-log", label:"Security Log"
+                    }
+                ]
+            },
+            {
+                key:"members-roles",
+                label:"Members & Roles"
+            }
+        ]
+    }
+
+
+]
+
 
 const Sidebar = () => {
     const [menuItem, setMenuItem] = useState(true);
+    const [itemActive, setItemActive] = useState({
+        subItemKey: "light"
+    })
+    const selectedItemFunction = (subItemKey: string) => {
+        setItemActive({ subItemKey: subItemKey })
+    }
+
+
+
     return (
 
         <div className="flex flex-col w-70 max-h-screen bg-white fixed">
-            <div className="px-[30px] py-[30px]">
+            <div className="px-6.5 py-[30px]">
                 <img src={MetronicLogo} alt="Metronic-Logo" />
             </div>
-            <div className="flex flex-col scrollbar-hidden">
+            <div className="flex flex-col scrollbar-hidden ">
                 <div className="flex flex-col pb-2.5">
                     <div className="flex flex-col gap-0 px-6.5">
                         {/* Başlık */}
-                        <div className="flex flex-row justify-between items-center py-3 pb-2">
+                        <div
+                            onClick={() => setMenuItem(!menuItem)}
+                            className="flex flex-row justify-between items-center py-3 pb-2 hover: group cursor-pointer"
+                        >
                             <div className="flex flex-row gap-2.5 items-center">
                                 <img src={Dashboard_Icon} alt="dashboard-icon" />
-                                <span className="text-gray-800 text-b-14-22-500">Dashboards</span>
+                                <span className="text-gray-800 text-b-14-22-500 group-hover:text-primary transition-colors">
+                                    Dashboards
+                                </span>
                             </div>
-                            <button onClick={() => setMenuItem(!menuItem)}>
-                                {
-                                    menuItem ? <>
-                                        <img src={Minus} alt="minus-icon" />
 
-                                    </> :
-                                        <>
-                                            <img src={Plus} alt="Plus-icon" />
-                                        </>
-                                }
-
-                            </button>
+                            {menuItem ? (
+                                <>
+                                    <img src={Minus} alt="minus-icon" />
+                                </>
+                            ) : (
+                                <>
+                                    <img src={Plus} alt="Plus-icon" />
+                                </>
+                            )}
                         </div>
 
-                        {/* Menü İçeriği */}
+
+                        {/* Submenu items */}
                         <div
                             className={`overflow-hidden transition-all duration-300 ${menuItem ? 'max-h-[150px] opacity-100' : 'max-h-0 opacity-0'
                                 }`}
                         >
-                            <div className="flex flex-col gap-2">
-                                <div className="flex items-center gap-4 bg-gray-100 rounded-md pl-3 py-2 relative z-10">
-                                    <span className="w-[6px] h-[6px] rounded-full bg-blue-500 ml-[-4px]"></span>
-                                    <span className="absolute w-[1px] h-full left-[10.5px] z-[-1] bg-gray-200"></span>
-                                    <span className="text-blue-500 text-b-13-14-400">Light Sidebar</span>
+                            <div className="flex flex-col gap-0">
+                                <div onClick={() => selectedItemFunction("light")} className={`subItem ${itemActive.subItemKey === "light" ? 'selectedItem' : 'unSelectedItem'}`}>
+                                    <span className={`${itemActive.subItemKey === "light" ? 'dotActive' : 'dotInActive'}`}></span>
+                                    <span className="itemDotLine"></span>
+                                    <span className={`${itemActive.subItemKey === "light" ? 'selectedItem' : 'unSelectedItem'} `}>Light Sidebar</span>
                                 </div>
 
-                                <div className="flex items-center gap-4 pl-3 py-2 relative z-10">
-                                    <span className="w-[6px] h-[6px] rounded-full bg-blue-500 ml-[-4px] bg-transparent"></span>
-                                    <span className="absolute w-[1px] h-full left-[10.5px] z-[-1] bg-gray-200"></span>
-                                    <span className="text-gray-800 text-b-13-14-400">Light Sidebar</span>
+                                <div onClick={() => selectedItemFunction("dark")} className={`subItem ${itemActive.subItemKey === "dark" ? 'selectedItem' : 'unSelectedItem'}`}>
+                                    <span className={`${itemActive.subItemKey === "dark" ? 'dotActive' : 'dotInActive'}`}></span>
+                                    <span className="itemDotLine"></span>
+                                    <span className={`${itemActive.subItemKey === "dark" ? 'selectedItem' : 'unSelectedItem'}`}>Dark Sidebar</span>
                                 </div>
 
-                                <div className="flex items-center gap-4 pl-3 py-2 relative z-10">
-                                    <span className="w-[6px] h-[6px] rounded-full bg-blue-500 ml-[-4px] bg-transparent"></span>
-                                    <span className="absolute w-[1px] h-full left-[10.5px] z-[-1] bg-gray-200"></span>
-                                    <span className="text-gray-800 text-b-13-14-400">Empty Dashboard</span>
-                                </div>
                             </div>
                         </div>
                     </div>
