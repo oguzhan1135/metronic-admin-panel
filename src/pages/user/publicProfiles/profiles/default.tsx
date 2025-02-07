a4529efdd227af9010dbf58c6904fdbe727da0bd
oguzhan1135 <111190495+oguzhan1135@users.noreply.github.com>
2025-02-07 19:08:19 +0300

    badge-about-unlock Creative area designed fixed #22

--- src/pages/user/publicProfiles/profiles/default.tsx
+++ src/pages/user/publicProfiles/profiles/default.tsx
@@ -8,6 +8,13 @@ import { CiMail } from 'react-icons/ci';
 import { FaChevronDown, FaUsers } from 'react-icons/fa';
 import { IoChatbubblesOutline } from 'react-icons/io5';
 import { BsThreeDotsVertical } from 'react-icons/bs';
+import BadgeOne from '../../../../assets/icon/badge-1.svg'
+import BadgeTwo from '../../../../assets/icon/badge-2.svg'
+import BadgeThree from '../../../../assets/icon/badge-3.svg'
+import BadgeFour from '../../../../assets/icon/badge-4.svg'
+import UnlockCreative from '../../../../assets/icon/unlockCreative.svg'
+import Chart from '../../../../components/charts/earningChart'
+
 
 
 const Default = () => {
@@ -21,7 +28,7 @@ const Default = () => {
                     <span>Jennifer Klabber</span>
                     <img src={Verify} alt="" />
                 </div>
-                <div className="flex flex-row items-center gap-5">
+                <div className="flex md:flex-row flex-col items-center gap-5">
                     <div className="flex flex-row items-center gap-[5px]">
                         <GiAbstract080 className='text-gray-600 text-b-14-14-500' />
                         <span className='text-gray-600 text-b-14-14-500'>KeenThemes</span>
@@ -36,33 +43,36 @@ const Default = () => {
                     </div>
                 </div>
             </div>
-            <div className="flex flex-row items-center justify-between border-b">
-                <div className="flex flex-row items-center gap-2.5">
-                    <div className="flex flex-row items-center gap-1 py-5 px-2.5">
-                        <span className='text-b-14-14-500 text-primary'>Profiles</span>
-                        <FaChevronDown className='text-b-14-14-500 text-primary' />
-                    </div>
-                    <div className="flex flex-row items-center gap-1 py-5 px-2.5">
-                        <span className='text-b-14-14-400 text-gray-700'>Projects</span>
-                        <FaChevronDown className='text-b-14-14-400 text-gray-700' />
-                    </div>
-                    <div className="flex flex-row items-center gap-1 py-5 px-2.5">
-                        <span className='text-b-14-14-400 text-gray-700'>Work</span>
-                    </div>
-                    <div className="flex items-center gap-1 py-5 px-2.5">
-                        <span className='text-b-14-14-400 text-gray-700'>Teams</span>
-                    </div>
-                    <div className="flex items-center gap-1 py-5 px-2.5">
-                        <span className='text-b-14-14-400 text-gray-700'>Network</span>
-                    </div>
-                    <div className="flex items-center gap-1 py-5 px-2.5">
-                        <span className='text-b-14-14-400 text-gray-700'>Activity</span>
-                    </div>
-                    <div className="flex flex-row items-center gap-1 py-5 px-2.5">
-                        <span className='text-b-14-14-400 text-gray-700'>More</span>
-                        <FaChevronDown className='text-b-14-14-400 text-gray-700' />
+            <div className="flex md:flex-row flex-col items-center md:justify-between border-b  justify-center overflow-hidden ">
+                <div className="w-full overflow-x-auto flex justify-center items-center">
+                    <div className="flex flex-row items-center gap-2.5 min-w-[250px]  ">
+                        <div className="flex flex-row items-center gap-1 py-5 px-2.5">
+                            <span className='text-b-14-14-500 text-primary'>Profiles</span>
+                            <FaChevronDown className='text-b-14-14-500 text-primary' />
+                        </div>
+                        <div className="flex flex-row items-center gap-1 py-5 px-2.5">
+                            <span className='text-b-14-14-400 text-gray-700'>Projects</span>
+                            <FaChevronDown className='text-b-14-14-400 text-gray-700' />
+                        </div>
+                        <div className="flex flex-row items-center gap-1 py-5 px-2.5">
+                            <span className='text-b-14-14-400 text-gray-700'>Work</span>
+                        </div>
+                        <div className="flex items-center gap-1 py-5 px-2.5">
+                            <span className='text-b-14-14-400 text-gray-700'>Teams</span>
+                        </div>
+                        <div className="flex items-center gap-1 py-5 px-2.5">
+                            <span className='text-b-14-14-400 text-gray-700'>Network</span>
+                        </div>
+                        <div className="flex items-center gap-1 py-5 px-2.5">
+                            <span className='text-b-14-14-400 text-gray-700'>Activity</span>
+                        </div>
+                        <div className="flex flex-row items-center gap-1 py-5 px-2.5">
+                            <span className='text-b-14-14-400 text-gray-700'>More</span>
+                            <FaChevronDown className='text-b-14-14-400 text-gray-700' />
+                        </div>
                     </div>
                 </div>
+
                 <div className="flex flex-row items-center gap-2.5">
                     <div className="px-2.5 py-2.5 flex flex-row items-center rounded-md bg-primary text-white gap-1">
                         <FaUsers />
@@ -77,6 +87,92 @@ const Default = () => {
                 </div>
             </div>
 
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-[30px] p-4">
+                {/* Sol Panel */}
+                <div className="md:col-span-1 flex flex-col gap-[30px]">
+                    <div className="bg-white  rounded-xl shadow-sm border">
+                        <div className="flex-col flex gap-5">
+                            <div className="border-b ">
+                                <div className="px-[30px] py-5">
+                                    <span className='text-b-16-16-600 text-gray-900'>Community Badges</span>
+                                </div>
+                            </div>
+                            <div className="flex flex-row items-center justify-start px-[30px] pb-[30px]">
+                                <img src={BadgeOne} alt="" />
+                                <img src={BadgeTwo} alt="" />
+                                <img src={BadgeThree} alt="" />
+                                <img src={BadgeFour} alt="" />
+
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="bg-white  rounded-xl shadow-sm border">
+                        <div className="flex-col flex gap-5">
+                            <div className="border-b ">
+                                <div className="px-[30px] py-5">
+                                    <span className='text-b-16-16-600 text-gray-900'>About</span>
+                                </div>
+                            </div>
+                            <div className="flex flex-row gap-4 pl-[30px] pb-[30px] pr-[50px]">
+                                <div className="flex flex-col  gap-5">
+                                    <span className='text-gray-600 text-b-14-14-400'>Age:</span>
+                                    <span className='text-gray-600 text-b-14-14-400'>City:</span>
+                                    <span className='text-gray-600 text-b-14-14-400'>State:</span>
+                                    <span className='text-gray-600 text-b-14-14-400'>Country:</span>
+                                    <span className='text-gray-600 text-b-14-14-400'>Postcode:</span>
+                                    <span className='text-gray-600 text-b-14-14-400'>Phone:</span>
+                                    <span className='text-gray-600 text-b-14-14-400'>Email:</span>
+
+                                </div>
+                                <div className="flex flex-col  gap-5">
+                                    <span className='text-b-14-14-400 text-gray-900'>32</span>
+                                    <span className='text-b-14-14-400 text-gray-900'>Amsterdam</span>
+                                    <span className='text-b-14-14-400 text-gray-900'>North Holland</span>
+                                    <span className='text-b-14-14-400 text-gray-900'>Netherlands</span>
+                                    <span className='text-b-14-14-400 text-gray-900'>1092 NL</span>
+                                    <span className='text-b-14-14-400 text-gray-900'>+31 6 1234 56 78</span>
+                                    <span className='text-b-14-14-400 text-gray-900'>jenny@ktstudio.com</span>
+
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div className="bg-white p-4 rounded-xl shadow-sm border">Work Experience</div>
+                    <div className="bg-white p-4 rounded-xl shadow-sm border">Skills</div>
+                </div>
+
+                <div className="md:col-span-2 flex flex-col gap-[30px]">
+                    <div className="bg-white rounded-xl shadow-sm border">
+                        <div className="flex md:flex-row flex-col items-center justify-between gap-10 px-10 py-[50px]">
+                            <div className="flex flex-col gap-3">
+                                <span className='text-gray-900 text-h-22-22-600 max-w-[250px]'>Unlock Creative
+                                    Partnerships on Our Blog</span>
+                                <p className='max-w-[437px] text-b-14-22-400 text-gray-700'>
+                                    Explore exciting collaboration opportunities with our blog. We're open to partnerships, guest posts, and more. Join us to share your insights and grow your audience.
+                                </p>
+                            </div>
+                            <img src={UnlockCreative} alt="" />
+
+                        </div>
+                    </div>
+                    <div className="bg-white rounded-xl shadow-sm border">
+                        <Chart />
+                    </div>
+
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-[30px]">
+                        <div className="bg-white rounded-xl shadow-sm border"></div>
+                        <div className="bg-white p-4 rounded-xl shadow-sm border">Assistance</div>
+                    </div>
+
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-[30px]">
+                        <div className="bg-white p-4 rounded-xl shadow-sm border">Contributors</div>
+                        <div className="bg-white p-4 rounded-xl shadow-sm border">Projects</div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
